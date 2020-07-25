fb392d1c6cf92b7edb8f68141fc009c844e97614
semye <464622832@qq.com>
2020-07-25 13:46:22 +0800

    commit

--- main.c
+++ main.c
@@ -164,7 +164,7 @@ void errorstr() {
     FILE *pf = fopen("unexist.txt", "rb");
     if (pf == NULL) {
         //stderr 按输出打印到屏幕
-        fprintf(stderr, strerror(errno));
+        fprintf(stderr, "%s", strerror(errno));
     }
 }
 
@@ -290,12 +290,735 @@ void bit_() {
 }
 
 
+/*void *calloc(int num, int size);
+在内存中动态地分配 num 个长度为 size 的连续空间，并将每一个字节都初始化为 0。所以它的结果是分配了 num*size 个字节长度的内存空间，并且每个字节的值都是0。*/
+
+/*	void free(void *address);
+该函数释放 address 所指向的内存块,释放的是动态分配的内存空间。*/
+
+/*void *malloc(int num);
+在堆区分配一块指定大小的内存空间，用来存放数据。这块内存空间在函数执行完成后不会被初始化，它们的值是未知的。*/
+
+/*void *realloc(void *address, int newsize);
+该函数重新分配内存，把内存扩展到 newsize。*/
+
+/*
+ * calloc函数的使用
+ */
+void calloc2() {
+    printf("%zu\n", sizeof(char));
+    char *p = calloc(5, sizeof(char));
+    if (p == NULL) {
+        perror("内存分配失败\n");
+        return;
+    }
+    char *des1 = "hepwdfdghjdfghjhkgdfsfghjhepwdfdghjdfghjhkgdfsfghjhepwdfdghjdfghjhkgdfsfghjhepwdfdghjdfghjhkgdfsfghjhepwdfdghjdfghjhkgdfsfghj";
+    strcpy(p, des1);
+    printf("=====>%s\n", p);
+    free(p);
+}
+
+/*
+ * 动态内存分配
+ */
+void malloc1() {
+    printf("动态内存分配");
+    char *des = malloc(200 * sizeof(char));
+    char *des1 = "hepwdfdghjdfghjhkgdfsfghj";
+    strcpy(des, des1);
+    printf("des===>%s\n", des);
+    //释放分配的内存
+    free(des);
+}
+
+/**
+ * c语言预处理器
+ * 内置宏
+ */
+void def() {
+    printf("当前日期:%s\n", __DATE__);
+    printf("当前时间:%s\n", __TIME__);
+    printf("当前时间戳:%s\n", __TIMESTAMP__);
+    printf("当前行数:%d\n", __LINE__);
+}
+
+void printerror() {
+    FILE *pf = fopen("unexist.txt", "rb");
+    if (pf == NULL) {
+        //stderr 按输出打印到屏幕
+        fprintf(stderr, "%s", strerror(errno));
+    }
+}
+// c语言中0为假 非0为真
+// 运算符
+// 算术运算符 + - * / % ++ --
+// 关系运算符  == != > >= < <=
+// 逻辑运算符 && || ！
+// 位运算符  & | ^ ~ 位运算符作用于位
+//  位运算真值表
+//  p	q	p & q	p | q	p ^ q
+//  0	0	0	    0	    0
+//  0	1	0	    1	    1
+//  1	1	1	    1	    0
+//  1	0	0	    1	    1
+//
+//
+// 赋值运算符 = += -= *= /= %= <<= >>= &= ^= |=
+// 杂项运算符  sizeof &取地址符  *指向变量 ?:
+
+//算术运算符
+void fu1n() {
+    int a = 21;
+    int b = 10;
+    int c;
+
+    c = a + b;
+    printf("Line 1 - c 的值是 %d\n", c);
+    c = a - b;
+    printf("Line 2 - c 的值是 %d\n", c);
+    c = a * b;
+    printf("Line 3 - c 的值是 %d\n", c);
+    c = a / b;
+    printf("Line 4 - c 的值是 %d\n", c);
+    c = a % b;
+    printf("Line 5 - c 的值是 %d\n", c);
+    c = a++;  // 赋值后再加 1 ，c 为 21，a 为 22
+    printf("Line 6 - c 的值是 %d\n", c);
+    c = a--;  // 赋值后再减 1 ，c 为 22 ，a 为 21
+    printf("Line 7 - c 的值是 %d\n", c);
+}
+
+//关系运算符
+void fun21() {
+
+    int a = 21;
+    int b = 10;
+    int c;
+
+    if (a == b) {
+        printf("Line 1 - a 等于 b\n");
+    } else {
+        printf("Line 1 - a 不等于 b\n");
+    }
+    if (a < b) {
+        printf("Line 2 - a 小于 b\n");
+    } else {
+        printf("Line 2 - a 不小于 b\n");
+    }
+    if (a > b) {
+        printf("Line 3 - a 大于 b\n");
+    } else {
+        printf("Line 3 - a 不大于 b\n");
+    }
+    /* 改变 a 和 b 的值 */
+    a = 5;
+    b = 20;
+    if (a <= b) {
+        printf("Line 4 - a 小于或等于 b\n");
+    }
+    if (b >= a) {
+        printf("Line 5 - b 大于或等于 a\n");
+    }
+}
+
+//逻辑运算符
+void fun31() {
+    int a = 5;
+    int b = 20;
+    int c;
+
+    if (a && b) {
+        printf("Line 1 - 条件为真\n");
+    }
+    if (a || b) {
+        printf("Line 2 - 条件为真\n");
+    }
+    /* 改变 a 和 b 的值 */
+    a = 0;
+    b = 10;
+    if (a && b) {
+        printf("Line 3 - 条件为真\n");
+    } else {
+        printf("Line 3 - 条件为假\n");
+    }
+    if (!(a && b)) {
+        printf("Line 4 - 条件为真\n");
+    }
+}
+
+/*位运算符*/
+void fun41() {
+    unsigned int a = 60;    /* 60 = 0011 1100 */
+    unsigned int b = 13;    /* 13 = 0000 1101 */
+    int c = 0;
+
+    c = a & b;       /* 12 = 0000 1100 */
+    printf("Line 1 - c 的值是 %d\n", c);
+
+    c = a | b;       /* 61 = 0011 1101 */
+    printf("Line 2 - c 的值是 %d\n", c);
+
+    c = a ^ b;       /* 49 = 0011 0001 */
+    printf("Line 3 - c 的值是 %d\n", c);
+
+    c = ~a;          /*-61 = 1100 0011 */
+    printf("Line 4 - c 的值是 %d\n", c);
+
+    c = a << 2;     /* 240 = 1111 0000 */
+    printf("Line 5 - c 的值是 %d\n", c);
+
+    c = a >> 2;     /* 15 = 0000 1111 */
+    printf("Line 6 - c 的值是 %d\n", c);
+}
+
+//赋值运算符
+void fun51() {
+    int a = 21;
+    int c;
+
+    c = a;
+    printf("Line 1 - =  运算符实例，c 的值 = %d\n", c);
+
+    c += a;
+    printf("Line 2 - += 运算符实例，c 的值 = %d\n", c);
+
+    c -= a;
+    printf("Line 3 - -= 运算符实例，c 的值 = %d\n", c);
+
+    c *= a;
+    printf("Line 4 - *= 运算符实例，c 的值 = %d\n", c);
+
+    c /= a;
+    printf("Line 5 - /= 运算符实例，c 的值 = %d\n", c);
+
+    c = 200;
+    c %= a;
+    printf("Line 6 - %s= 运算符实例，c 的值 = %d\n", "%", c);
+
+    c <<= 2;
+    printf("Line 7 - <<= 运算符实例，c 的值 = %d\n", c);
+
+    c >>= 2;
+    printf("Line 8 - >>= 运算符实例，c 的值 = %d\n", c);
+
+    c &= 2;
+    printf("Line 9 - &= 运算符实例，c 的值 = %d\n", c);
+
+    c ^= 2;
+    printf("Line 10 - ^= 运算符实例，c 的值 = %d\n", c);
+
+    c |= 2;
+    printf("Line 11 - |= 运算符实例，c 的值 = %d\n", c);
+}
+
+
+
+void bubble_sort(int array[], int num) {
+    int temp;
+    for (int j = 0; j < num - 1; j++) {
+        for (int i = 0; i < num - 1 - j; i++) {
+            if (array[i] > array[i + 1]) {
+                //交换两个数字的位置
+                temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+
+    for (int k = 0; k < num; k++) {
+        printf("%d ", array[k]);
+    }
+}
+
+/*
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，
+ * 然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 以此类推，直到所有元素均排序完毕。
+ */
+void selection_sort(int array[], int num) {
+    for (int i = 0; i < num - 1; i++) {
+        int m = i;
+        for (int j = i + 1; j < num; j++) {
+            if (array[j] < array[m]) {
+                m = j;
+            }
+        }
+        int temp = array[m];
+        array[m] = array[i];
+        array[i] = temp;
+    }
+    for (int k = 0; k < num; k++) {
+        printf("%d ", array[k]);
+    }
+}
+
+
+/*
+ * 插入排序
+ */
+void insertion_sort(int array[], int num) {
+    for (int i = 1; i < num; i++) {
+        for (int j = i; j > 0 && array[j] < array[j - 1]; j--) {
+            int temp = array[j];
+            array[j] = array[j - 1];
+            array[j - 1] = temp;
+        }
+    }
+    for (int k = 0; k < num; k++) {
+        printf("%d ", array[k]);
+    }
+}
+
+/*
+ * 第一步：申请空间，使其大小为两个已经排序序列之和，该空间用来存放合并后的序列
+ * 第二步：设定两个指针，最初位置分别为两个已经排序序列的起始位置
+ * 第三步：比较两个指针所指向的元素，选择相对小的元素放入到合并空间，并移动指针到下一位置
+ * 重复步骤3直到某一指针超出序列尾
+ * 将另一序列剩下的所有元素直接复制到合并序列尾
+ */
+void merge_sort(int array1[], int array2[], int num) {
+
+}
+
+/*
+ * 快速排序
+ */
+void quick_sort(int array[], int mum) {
+
+}
+
+/* 冒泡排序*/
+void sort1() {
+    int a[] = {52, 19, 28, 32, 4, 5, 6, 7, 8, 9, 0, 22, 55, 43, 15};
+    int num = sizeof(a) / sizeof(int);
+    printf("冒泡排序\n");
+    bubble_sort(a, num);
+}
+
+/*
+ * 选择排序
+ */
+void sort2() {
+    int b[] = {52, 19, 28, 32, 4, 5, 6, 7, 8, 9, 0, 22, 55, 43, 15};
+    int num1 = sizeof(b) / sizeof(int);
+    printf("选择排序\n");
+    selection_sort(b, num1);
+}
+
+/*
+ * 插入排序
+ */
+void sort3() {
+    printf("插入排序\n");
+    int c[] = {52, 19, 28, 32, 4, 5, 6, 7, 8, 9, 0, 22, 55, 43, 15};
+    int num2 = sizeof(c) / sizeof(int);
+    insertion_sort(c, num2);
+}
+
+
+
+
+
+/*
+ * 变量的值改变,内存地址不会改变,所以可以操作指针来改变变量的值
+ * 定义指针时,如果没有确切的地址定义成空指针是一个很好的习惯
+ * 能接受指针作为参数的函数，也能接受数组作为参数
+ */
+void fun1111() {
+    int a = 3;
+    int *p = &a;
+    int *p1 = NULL;
+    printf("内存地址%p\n", p1);
+    printf("内存地址%p\n", p);
+    a = 4;
+    printf("内存地址%p\n", p);
+    *p = 5;
+    printf("a=%d\n", a);
+    if (p) {
+        printf("hello world!\n");
+    }
+}
+
+const int MAX = 3;
+
+/*指针的运算 递增指针*/
+void fun2111() {
+    int var[] = {10, 20, 30};
+    int i, *p;
+    p = var;
+    for (i = 0; i < MAX; i++) {
+        printf("var[%d]=%p\n", i, p);
+        printf("var[%d]=%d\n", i, *p);
+        p++;
+    }
+}
+
+//指针的比较
+//如果 p1 和 p2 指向两个相关的变量，比如同一个数组中的不同元素，则可对 p1 和 p2 进行大小比较。
+void fun3111() {
+    int var[] = {10, 20, 30};
+    int *p1 = &var[2];
+    int *p2 = &var[0];
+    if (p2 < p1) {
+        printf("指针的比较结果:p2<p1\n");
+    }
+}
+
+//指针数组
+void fun4() {
+    int var[] = {10, 20, 30};
+    int *ptr[MAX];
+    for (int i = 0; i < MAX; ++i) {
+        printf("%d\n", i);
+        ptr[i] = &var[i];
+    }
+    for (int j = 0; j < MAX; ++j) {
+        printf("Value of var[%d] = %d\n", j, *ptr[j]);
+    }
+}
+
+//指向指针的指针
+void fun5() {
+    int a = 100;
+    int *p = &a;
+    int **p1 = &p;
+    printf("存储变量a的内存地址%p\n", &a);
+    printf("存储指针变量p的地址%p\n", &p);
+    printf("存储指针变量p1的地址%p\n", &p1);
+
+}
+
+//传递指针给函数
+void fun6(int *p) {
+    printf("打印指针保存的变量的内存地址:%p", p);
+}
+
+
+//从函数返回指针
+int *fun7() {
+    static int r[10];
+    /* 随机函数 设置种子 */
+    srand((unsigned int) time(NULL));
+    for (int j = 0; j < 10; ++j) {
+        r[j] = rand();
+        printf("%d\n", r[j]);
+    }
+    return r;
+}
+
+//函数指针
+int maxXy(int x, int y) {
+    return x > y ? x : y;
+}
+
+//函数指针的使用
+int fun8() {
+    //定义一个函数指针
+    int (*p)(int, int)=&maxXy;
+    //通过函数指针调用函数
+    int s = p(1, 2);
+    printf("%d\n", s);
+}
+
+void callyou(void) {
+    printf("东西到货了,可以来拿了\n");
+}
+
+void callexecute(void *p()) {
+    printf("东西没货,等到货再通知你\n");
+    p();
+}
+
+//回调函数的使用
+int fun9() {
+    void (*p) = &callyou;
+    callexecute(p);
+}
+
+/*
+ * 指针交换两个变量
+ */
+void swapp(int *p1, int *p2) {
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+
+/*
+ * 值交换
+ */
+void swapv(int v1, int v2) {
+    int temp = v1;
+    v1 = v2;
+    v2 = temp;
+}
+
+//指向数组的指针
+void fun() {
+    /* 带有 5 个元素的整型数组 */
+    double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+    double *p;
+    int i;
+
+    p = balance;
+
+    /* 输出数组中每个元素的值 */
+    printf("使用指针的数组值\n");
+    for (i = 0; i < 5; i++) {
+        printf("*(p + %d) : %f\n", i, *(p + i));
+    }
+
+    printf("使用 balance 作为地址的数组值\n");
+    for (i = 0; i < 5; i++) {
+        printf("*(balance + %d) : %f\n", i, *(balance + i));
+    }
+
+}
+
+
+
+
+// 数据类型
+// 各种类型的存储大小与系统位数有关，但目前通用的以64位系统为主
+// 1字节 8位
+// float 单精度浮点值。单精度是这样的格式，1位符号，8位指数，23位小数。
+// double 双精度浮点值。双精度是1位符号，11位指数，52位小数。
+//
+//
+// 基本类型 整数类型和浮点类型
+//
+//         整数类型
+//         类型            存储大小        值范围
+//         char            1字节           -128~127 或0到255
+//         unsigned char   1字节           0到255
+//         signed char     1字节           -128~127
+//         int             2 或 4 字节  2的31次方-1    -32,768 到 32,767 或 -2,147,483,648 到 2,147,483,647
+//         unsigned int    2 或 4 字节     0到65,535 或 0到4,294,967,295
+//         short           2 字节          -32,768 到 32,767
+//         unsigned short  2 字节          0到 65,535
+//         long            4 或 8 字节          -2,147,483,648 到 2,147,483,647
+//         unsigned long   4 或 8 字节          0到4,294,967,295
+//
+//         浮点类型
+//         类型            存储大小        值范围                    精度
+//         float           4 字节         1.2E-38 到 3.4E+38       6 位小数
+//         double          8 字节         2.3E-308 到 1.7E+308     15 位小数
+//         long double     16 字节        3.4E-4932 到 1.1E+4932   19 位小数
+//
+// void类型
+// 枚举类型
+// 派生类型 指针类型、数组类型、结构类型、共用体类型和函数类型
+//
+
+
+
+
+
+const int b = 1;//定义常量
+
+
+/*
+ * 强制类型转换
+ * double ←── float 高
+ * ↑
+ * long
+ * ↑
+ * unsigned
+ * ↑
+ * int ←── char,short 低
+ *
+ * float和double 默认打印都是小数点后6位
+ */
+void type_() {
+    float f1 = 1.2f;
+    printf("%f\n", f1);
+    //通过%.nf可控制打印的小数点个数
+    printf("%.2f\n", f1);
+
+    double d1 = 1.222222222f;
+    double d2 = 1.222222222f;
+    double d3 = d1 * d2;
+    printf("%f\n", d3);
+}
+
+// 枚举 数据类型
+enum DAY {
+    MON = 1, TUE, WEB, THU, FRI, SAT, SUN
+} day;
+
+
+// c 文件读写
+//
+// fopen打开一个文件或者创建一个文件
+// mode
+// r 打开一个已有的文本文件，允许读取文件
+// w 打开一个文本文件，允许写入文件。如果文件不存在，则会创建一个新文件。在这里，您的程序会从文件的开头写入内容。如果文件存在，则该会被截断为零长度，重新写入。
+// a 打开一个文本文件，以追加模式写入文件。如果文件不存在，则会创建一个新文件。在这里，您的程序会在已有的文件内容中追加内容。
+// r+ 打开一个文本文件，允许读写文件。
+// w+ 打开一个文本文件，允许读写文件。如果文件已存在，则文件会被截断为零长度，如果文件不存在，则会创建一个新文件。
+// a+ 打开一个文本文件，允许读写文件。如果文件不存在，则会创建一个新文件。读取会从文件的开头开始，写入则只能是追加模式。
+
+//如果文件是二进制文件 mode 后需要加个b,b是二进制的英文
+
+//"rb", "wb", "ab", "rb+", "r+b", "wb+", "w+b", "ab+", "a+b"
+
+
+
+/*
+ * 创建文件
+ */
+void create_file(char *filename) {
+    FILE *file = fopen(filename, "a+");
+    fclose(file);
+}
+
+//fputc 写入字符
+//fputs 写入字符串
+void write_file() {
+    FILE *file = fopen("123.txt", "a+");
+    fputc(99, file);
+    fputs("hello", file);
+    fclose(file);
+}
+
+//fgetc 读取字符
+//fgets 读取字符串
+void read_file() {
+    FILE *file = fopen("123.txt", "r");
+    int c = fgetc(file);
+    printf("%c\n", c);
+    char ccc[100];
+    fgets(ccc, 1024, file);
+    printf("%s\n", ccc);
+    fclose(file);
+
+}
+
+/*
+ * 打开文件
+ */
+void open_file_use() {
+    FILE *a1 = fopen("1111.txt", "rb");
+    if (!a1) {
+        perror("打开文件1111.txt失败\n");
+    } else {
+        printf("打开文件1111.txt成功\n");
+        //用于保存读取的数据
+        char a[1024];
+        size_t ret_code = fread(a, sizeof *a, 1024, a1);
+        printf("读取的字符====>%zu\n", ret_code);
+        printf("文本的内容为====>%s\n", a);
+        fclose(a1);
+    }
+
+}
+
+#define READ_BUFF 100
+
+
+void copy_file(char *filename, char *copy_filename) {
+    FILE *fp1, *fp2;
+    size_t c = 0;
+    char buf[READ_BUFF];
+    fp1 = fopen(filename, "rb");
+    fp2 = fopen(copy_filename, "wb");
+    if (fp1 == NULL || fp2 == NULL) {
+        printf("open %s or %s failed:%s\n", filename, copy_filename, strerror(errno));
+        return;
+    }
+    while ((c = (int) fread(buf, sizeof(char), READ_BUFF, fp1)) > 0) {
+        fwrite(buf, sizeof(size_t), c, fp2);
+    }
+    fclose(fp1);
+    fclose(fp2);
+}
+
+
+
+
+
+//getchar 从键盘获取字符
+//putchar 显示到屏幕
+void fun999() {
+    int c = getchar();
+    putchar(c);
+    printf("\n====> %d", c);
+}
+
+
+// gets 从键盘获取字符串
+// puts 显示到屏幕
+void fun2999() {
+    char abc[100];
+    gets(abc);
+    puts(abc);
+}
+
+// c 语言 存储类
+// auto 存储类是所有局部变量默认的存储类
+// register 存储类用于定义存储在寄存器中而不是 RAM 中的局部变量 这意味着变量的最大尺寸等于寄存器的大小（通常是一个词），且不能对它应用一元的 '&' 运算符（因为它没有内存位置）。
+// static 存储类指示编译器在程序的生命周期内保持局部变量的存在，而不需要在每次它进入和离开作用域时进行创建和销毁。因此，使用 static 修饰局部变量可以在函数调用之间保持局部变量的值。
+//
+//static 修饰符也可以应用于全局变量。当 static 修饰全局变量时，会使变量的作用域限制在声明它的文件内。
+//
+//全局声明的一个 static 变量或方法可以被任何函数或方法调用，只要这些方法出现在跟 static 变量或方法同一个文件中。
+// extern 存储类用于提供一个全局变量的引用，全局变量对所有的程序文件都是可见的。当您使用 extern 时，对于无法初始化的变量，会把变量名指向一个之前定义过的存储位置。
+//
+//当您有多个文件且定义了一个可以在其他文件中使用的全局变量或函数时，可以在其他文件中使用 extern 来得到已定义的变量或函数的引用。可以这么理解，extern 是用来在另一个文件中声明一个全局变量或函数。
+//
+//extern 修饰符通常用于当有两个或多个文件共享相同的全局变量或函数的时候
+
+
+
+/*
+ * 四种存储类
+ * auto 所有局部变量的默认存储类 默认保存在RAM中
+ * register 定义的局部变量可能保存在寄存器中 不能对它应用一元的 '&' 运算符（因为它没有内存位置）
+ * static 可修饰局部变量和全局变量  在修饰局部变量时，多次调用会无效
+ * extern 提供一个全局变量的引用
+ */
+void funyy() {
+    {
+        int month;
+        auto int year;
+    }
+    int day = 2;
+    printf("%d\n", day);
+    register int *ye;
+    //   不能对 register的局部变量使用& 因为没有内存位置
+    // printf("%p\n", &ye);
+
+
+    static int ss = 9;
+    printf("%d\n", ss);
+    ss--;
+    printf("%d\n", ss);
+}
+
+
+extern int yesheng; //extern 提供全局的引用
+
+/*
+ * time函数的使用
+ * 当前的时间戳 秒数
+ */
+time_t get_current_time() {
+    time_t time_t = time(NULL);
+    printf("当前时间%li\n", time_t);
+    return time_t;
+}
+
+
+
 
 
 
 //argc 参数个数
 //argv[0] 存储程序的名称
 int main(int argc, char *argv[]) {
+    printerror();
     fun3();
     unsigned char a = '%';
     printf("%d\n", a);
